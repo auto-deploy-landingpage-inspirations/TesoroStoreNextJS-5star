2022f8a43455f8d9886459a0670cfa25888768da
Prashant Mishra <pgmd.9873@gmail.com>
2024-01-06 10:04:09 +0530

    privacy policy updated

--- src/settings/privacy-settings.ts
+++ src/settings/privacy-settings.ts
@@ -29,4 +29,34 @@ export const privacyPolicy = [
     title: "privacy-six-title",
     description:"privacy-six-content",
   },
+  {
+    id: "7",
+    title: "privacy-seven-title",
+    description:"privacy-seven-content",
+  },
+  {
+    id: "8",
+    title: "privacy-eight-title",
+    description: "privacy-eight-content",
+  },
+  {
+    id: "9",
+    title: "privacy-nine-title",
+    description:"privacy-nine-content",
+  },
+  {
+    id: "10",
+    title: "privacy-ten-title",
+    description:"privacy-ten-content",
+  },
+  {
+    id: "11",
+    title: "privacy-eleven-title",
+    description:"privacy-eleven-content",
+  },
+  {
+    id: "12",
+    title: "privacy-twelve-title",
+    description:"privacy-twelve-content",
+  }
 ];
