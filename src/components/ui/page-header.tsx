45627ee80917c8208be146baed932b91521c9b10
Prashant Mishra <pgmd.9873@gmail.com>
2024-01-10 03:58:40 +0530

    updated refund policy page

--- src/components/ui/page-header.tsx
+++ src/components/ui/page-header.tsx
@@ -20,9 +20,9 @@ const PageHeader: React.FC<HeaderProps> = ({
 			<div className="absolute top-0 start-0 bg-black w-full h-full opacity-50 transition-opacity duration-500 group-hover:opacity-80" />
 			<div className="w-full flex items-center justify-center relative z-10 py-10 md:py-14 lg:py-20 xl:py-24 2xl:py-32">
 				<h2 className="text-xl md:text-2xl lg:text-3xl font-bold text-white text-center">
-					<span className="font-satisfy block font-normal mb-3">
+					{/* <span className="font-satisfy block font-normal mb-3">
 						{t(`${pageSubHeader}`) as string}
-					</span>
+					</span> */}
 					{t(`${pageHeader}`) as string}
 				</h2>
 			</div>
--- src/pages/refund-policy-old.tsx
+++ src/pages/refund-policy-old.tsx
@@ -1,34 +1,24 @@
 import Layout from "@components/layout/layout";
 import Container from "@components/ui/container";
 import PageHeader from "@components/ui/page-header";
-import Iframe from "react-iframe";
-// import { privacyPolicy } from "@settings/privacy-settings";
-// import { Link, Element } from "react-scroll";
+import { privacyPolicy } from "@settings/privacy-settings";
+import { Link, Element } from "react-scroll";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-// import { useTranslation } from "next-i18next";
+import { useTranslation } from "next-i18next";
 import { GetStaticProps } from "next";
 
-// function makeTitleToDOMId(title: string) {
-// 	return title.toLowerCase().split(" ").join("_");
-// }
+function makeTitleToDOMId(title: string) {
+	return title.toLowerCase().split(" ").join("_");
+}
 
-export default function PrivacyPage() {
-	// const { t } = useTranslation("privacy");
+export default function RefundPolicy() {
+	const { t } = useTranslation("privacy");
 	return (
 		<>
 			<PageHeader pageHeader="Refund Policy" />
-			<div style={{height: '90vh'}} className="mt-12 lg:mt-14 xl:mt-16 lg:py-1 xl:py-0 border-b border-gray-300 px-4 md:px-10 lg:px-7 xl:px-16 2xl:px-24 3xl:px-32 pb-9 md:pb-14 lg:pb-16 2xl:pb-20 3xl:pb-24 h-[80vh]">
-				<Container className="h-[80vh]">
-				<Iframe 
-					url="https://docs.google.com/document/d/e/2PACX-1vSsRAq_wwvK6WpSxuNOXNJsvx4U47XweBeuNJOXQczpQgWpfuKbp_9IAtFkVqLlRhZYfqpMf2WR2xiw/pub?embedded=true"
-					width="90%"
-					height="100%"
-					id="something_new"
-					className=""
-					display="block"
-					position="relative"/>
-				{/* <iframe src="" className="w-full h-[100%]" style={{height: '100vh'}}></iframe> */}
-					{/* <div className="flex flex-col md:flex-row">
+			<div className="mt-12 lg:mt-14 xl:mt-16 lg:py-1 xl:py-0 border-b border-gray-300 px-4 md:px-10 lg:px-7 xl:px-16 2xl:px-24 3xl:px-32 pb-9 md:pb-14 lg:pb-16 2xl:pb-20 3xl:pb-24">
+				<Container>
+					<div className="flex flex-col md:flex-row">
 						<nav className="md:w-72 xl:w-3/12 mb-8 md:mb-0">
 							<ol className="sticky md:top-16 lg:top-28 z-10">
 								{privacyPolicy?.map((item, index) => (
@@ -51,6 +41,7 @@ export default function PrivacyPage() {
 								))}
 							</ol>
 						</nav>
+						{/* End of section scroll spy menu */}
 
 						<div className="md:w-9/12 md:ps-8 pt-0 lg:pt-2">
 							{privacyPolicy?.map((item) => (
@@ -72,14 +63,15 @@ export default function PrivacyPage() {
 								</Element>
 							))}
 						</div>
-					</div> */}
+						{/* End of content */}
+					</div>
 				</Container>
 			</div>
 		</>
 	);
 }
 
-PrivacyPage.Layout = Layout;
+RefundPolicy.Layout = Layout;
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
 	return {
