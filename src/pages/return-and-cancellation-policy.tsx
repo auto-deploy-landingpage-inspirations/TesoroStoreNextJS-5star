b032bdb7163f5acbc6e986372b989cb644a9a450
prashant9316 <pgmd.9873@gmail.com>
2024-05-07 18:28:32 +0530

    updated favicon

--- src/pages/return-and-cancellation-policy.tsx
+++ src/pages/return-and-cancellation-policy.tsx
@@ -26,53 +26,8 @@ export default function ReturnPage() {
 					id="something_new"
 					className="ml-[2%]"
 					display="block"
-					position="relative"/>
-				{/* <iframe src="" className="w-full h-[100%]" style={{height: '100vh'}}></iframe> */}
-					{/* <div className="flex flex-col md:flex-row">
-						<nav className="md:w-72 xl:w-3/12 mb-8 md:mb-0">
-							<ol className="sticky md:top-16 lg:top-28 z-10">
-								{privacyPolicy?.map((item, index) => (
-									<li key={item.id}>
-										<Link
-											spy={true}
-											offset={-120}
-											smooth={true}
-											duration={500}
-											to={makeTitleToDOMId(item.title)}
-											activeClass="text-heading font-semibold"
-											className="block cursor-pointer py-3 lg:py-3.5  text-sm lg:text-base  text-gray-700 uppercase"
-										>
-											{(index <= 9 ? "0" : "") +
-												index +
-												" " +
-												t(`${item.title}`)}
-										</Link>
-									</li>
-								))}
-							</ol>
-						</nav>
-
-						<div className="md:w-9/12 md:ps-8 pt-0 lg:pt-2">
-							{privacyPolicy?.map((item) => (
-								// @ts-ignore
-								<Element
-									key={item.title}
-									id={makeTitleToDOMId(item.title)}
-									className="mb-10"
-								>
-									<h2 className="text-lg md:text-xl lg:text-2xl text-heading font-bold mb-4">
-										{t(`${item.title}`)as string}
-									</h2>
-									<div
-										className="text-heading text-sm leading-7 lg:text-base lg:leading-loose"
-										dangerouslySetInnerHTML={{
-											__html: t(`${item.description}`),
-										}}
-									/>
-								</Element>
-							))}
-						</div>
-					</div> */}
+					position="relative"
+				/>
 				</Container>
 			</div>
 		</>
